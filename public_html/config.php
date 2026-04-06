b915023373978e3a8f08666fb1b2fa3a49c0947b
toesan <67867354+toes4n@users.noreply.github.com>
2026-04-06 22:59:11 +0700

    Update config.php

--- public_html/config.php
+++ public_html/config.php
@@ -4,10 +4,10 @@ unset($CFG);
 global $CFG;
 $CFG = new stdClass();
 
-$CFG->dbtype    = 'mariadb'; // Use 'mysqli' if you encounter issues, though mariadb often works
+$CFG->dbtype    = 'mariadb'; // Use 'mysqli' if 'mariadb' causes issues on Azure MySQL
 $CFG->dblibrary = 'native';
-$CFG->dbhost    = 'who-srv.mysql.database.azure.com'; // Your Azure Endpoint
-$CFG->dbname    = 'u999747919_lmsdemo2';             // Ensure this DB exists on Azure
+$CFG->dbhost    = 'who-srv.mysql.database.azure.com';
+$CFG->dbname    = 'u999747919_lmsdemo2'; // Ensure this DB name exists on the Azure server
 $CFG->dbuser    = 'tkqweddfmy';
 $CFG->dbpass    = 'OUMPdHotFUTe5$do';
 $CFG->prefix    = 'mdl_';
@@ -16,20 +16,17 @@ $CFG->dboptions = array (
   'dbport' => 3306,
   'dbsocket' => '',
   'dbcollation' => 'utf8mb4_unicode_ci',
-  'dbssl' => true, // Azure MySQL Flexible Server requires SSL by default
+  'dbssl' => true, // Azure Flexible Server strongly recommends SSL
 );
 
-// Update this to your Azure Default Domain from the screenshot
-$CFG->wwwroot   = 'https://who-gta0ccdvd2g4e8ge.southeastasia-01.azurewebsites.net';
+// Use your custom domain for the wwwroot
+$CFG->wwwroot   = 'https://who.tsandev.org';
 
-// On Azure App Service, the web root is typically /home/site/wwwroot
-// Ensure you have created the 'moodledata' folder in /home/
-$CFG->dataroot  = '/home/moodledata'; 
+// Path for Azure App Service (usually /home/site/wwwroot/moodledata)
+// Make sure this directory is outside your public web folder and is writable
+$CFG->dataroot  = '/home/data/moodledata'; 
 
 $CFG->admin     = 'admin';
-$CFG->directorypermissions = 0777;
+$CFG->directorypermissions = 02777; // Standard for Azure App Service linux environments
 
 require_once(__DIR__ . '/lib/setup.php');
-
-// There is no php closing tag in this file,
-// it is intentional because it prevents trailing whitespace problems!
